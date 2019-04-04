1a15e68fe793529f09efcaa83954ffbc2dc70c61
dougwright <douglas.wright@ovo.com.au>
2019-04-04 15:40:40 +1100

    Add new Java example app with AndroidX dependencies

---
+++ sourse-example-java-androidx/app/src/main/java/co/sourse/example/pvn/ExampleActivity.java
@@ -0,0 +1,15 @@
+package co.sourse.example.pvn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import co.sourse.example.R;
+
+import android.os.Bundle;
+
+public class ExampleActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
